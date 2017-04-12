29c495632fc152227a304d717a0a665a13d28725
Naz <nazmalik08@gmail.com>
2017-04-12 10:17:22 +0100

    Add increase function and test for decrease function

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -10,7 +10,19 @@ describe('Thermostat', function() {
     it('starts at twenty degrees', function() {
       expect(thermostat.temp).toEqual(20);
     });
+  });
 
+  describe('changing temp', function() {
+  
+    it('increases temp with an increase function', function() {
+      thermostat.increase();
+      expect(thermostat.temp).toEqual(21);
+    });
+
+    it('decreases temp with a decrease function', function(){
+      thermostat.decrease();
+      expect(thermostat.temp).toEqual(19);
+    });
   });
 
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,7 @@
 function Thermostat(temp=20) {
   this.temp = temp
 }
+
+Thermostat.prototype.increase = function(){
+  this.temp += 1
+};
