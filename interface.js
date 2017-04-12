8323d83e4bb18d20d7579d114ac433b899c0e569
Kate <allbecauseyoutoldmeso@yahoo.co.uk>
2017-04-12 18:16:05 +0100

    add weather api

--- interface.js
+++ interface.js
@@ -1,6 +1,13 @@
 $(document).ready(function() {
   var thermostat = new Thermostat;
   updates();
+  displayWeather('bratislava')
+
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city)
+  })
 
   $('#increase-temperature').on('click',function(){
     thermostat.increase();
@@ -35,4 +42,14 @@ $(document).ready(function() {
     updateTemperature();
     updateUsage();
   }
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=';
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed';
+    var units = '&units=metric';
+    $.get(url + city + token + units, function(data) {
+      $('#current-temperature').text(data.main.temp)
+    })
+  }
+
 });
